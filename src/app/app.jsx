3f17deb29969c88c441a10ce891f84142baa02b9
Narek <narek1985ch@gmail.com>
2022-01-10 21:16:36 +0100

    added errorPage component for non-existent pages

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navBar";
 import Main from "./components/main";
 import Login from "./components/login";
 import Users from "./components/users";
 import User from "./components/user";
+import ErrorPage from "./components/errorPage";
 
 const App = () => {
    return (
@@ -14,7 +15,9 @@ const App = () => {
             <Route path="/login" component={Login} />
             <Route path="/users/:id" component={User} />
             <Route path="/users" component={Users} />
-            <Route path="/" component={Main} />
+            <Route path="/" exact component={Main} />
+            <Route path="/404" exact component={ErrorPage} />
+            <Redirect to="/404" />
          </Switch>
       </>
    );
---
+++ src/app/components/errorPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+   return (
+      <>
+         <h1>{"404"}</h1>
+         <h4>{"This page does not exist."}</h4>
+      </>
+   );
+};
+
+export default ErrorPage;
