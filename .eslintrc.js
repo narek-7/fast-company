f9208efd0c0591547713f8016b20de0e53c72ffd
Narek <narek1985ch@gmail.com>
2022-01-16 11:34:42 +0100

    Defined validators for login and password

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ["plugin:react/recommended", "standard"],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 13,
-		sourceType: "module",
-	},
-	plugins: ["react"],
-	rules: {
-		indent: ["error", 3], // tab = 3
-		semi: [2, "always"], // " "
-		"space-before-function-paren": [
-			"error",
-			{ anonymous: "always", named: "never" },
-		], // function()
-		quotes: ["error", "double", { allowTemplateLiterals: true }], //
-	},
+   env: {
+      browser: true,
+      es2021: true
+   },
+   extends: ["plugin:react/recommended", "standard"],
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true
+      },
+      ecmaVersion: 13,
+      sourceType: "module"
+   },
+   plugins: ["react"],
+   rules: {
+      indent: ["error", 3], // tab = 3
+      semi: [2, "always"], // " "
+      "space-before-function-paren": [
+         "error",
+         { anonymous: "always", named: "never" }
+      ], // function()
+      quotes: ["error", "double", { allowTemplateLiterals: true }] //
+   }
 };
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "../components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
    const [data, setData] = useState({ email: "", password: "" });
+   const [errors, setErrors] = useState({});
+
    const handleChange = ({ target }) => {
       setData((prevState) => ({
          ...prevState,
@@ -10,8 +13,40 @@ const Login = () => {
       }));
    };
 
+   const validatorConfig = {
+      email: {
+         isRequired: { message: "Email is Required" },
+         isEmail: { message: "Email is incorrect" }
+      },
+      password: {
+         isRequired: { message: "Password is Required" },
+         isCapitalSymbol: {
+            message: "Must contain at least one capital letter"
+         },
+         isContainDigit: {
+            message: "Must contain at least one digit"
+         },
+         min: {
+            message: "Password must be at least 8 characters",
+            value: 8
+         }
+      }
+   };
+
+   useEffect(() => {
+      validate();
+   }, [data]);
+
+   const validate = () => {
+      const errors = validator(data, validatorConfig);
+      setErrors(errors);
+      return Object.keys(errors).length === 0;
+   };
+
    const handleSubmit = (e) => {
       e.preventDefault();
+      const isValid = validate();
+      if (!isValid) return;
       console.log(data);
    };
 
@@ -22,6 +57,7 @@ const Login = () => {
             name="email"
             value={data.email}
             onChange={handleChange}
+            error={errors.email}
          />
          <TextField
             label="Password"
@@ -29,6 +65,7 @@ const Login = () => {
             name="password"
             value={data.password}
             onChange={handleChange}
+            error={errors.password}
          />
          {/* for creating not submit button, we need to change the type of  the button */}
          <button type="submit">Submit</button>
